afbd8eab5497c2d931b7661a39b8ef7f2c534c95
Alessandro Clerici <ale.clericil@gmail.com>
2021-08-14 13:26:47 +0200

    first commit: added notes

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# notes
+# default: create the switched version of the current keymap and install
+# if argument present: create the switched version of the specified keymap
+# option to (not) set as default keymap
+# option to (not) automatically activate at the end
+# verbose (tells the user where it put the new file)
